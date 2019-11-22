f7f7b49534a1220bdebef498e042dee5ee2e7466
ZeyuanZhang <zeyuan.zhang@tum.de>
2019-11-23 00:17:29 +0100

    2nd version

---
+++ Project/task/Settings.py
@@ -0,0 +1,13 @@
+path = '/home/ge34caj/Downloads/pku-autonomous-driving/'
+mode = 'TRAINING'
+device = 'cuda'
+
+epoch = 1
+
+img_width = 1024
+model_scale = 8
+distance_thresh_clear = 2
+
+data_sample_factor = 0.1
+lr = 1e-3
+batch_size = 4
